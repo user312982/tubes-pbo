539e43910e4bd83a365f1d57698e36ed2b7fbe37
andizalfa <11231010@student.itk.ac.id>
2024-05-28 13:19:24 +0800

    adding MapelPage action button change scene and MenuPage profile name user login
    Signed-off-by: andizalfa <11231010@student.itk.ac.id>

--- src/main/java/com/example/tubespbo/MapelPage.java
+++ src/main/java/com/example/tubespbo/MapelPage.java
@@ -1,4 +1,42 @@
 package com.example.tubespbo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import static com.example.tubespbo.UserData.getInstance;
+
 public class MapelPage {
+
+    @FXML
+    private Button profileButton;
+    
+    private Main main;
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    @FXML
+    private void profileButtonAction(ActionEvent event) throws IOException {
+        main.switchScene("MenuPageDesign.fxml");
+    }
+
+    @FXML
+    private void jadwalButtonAction(ActionEvent event) throws IOException {
+        main.switchScene("JadwalPageDesign.fxml");
+    }
+
+    public void setData() {
+        profileButton.setText(" " + getInstance().storedUsername);
+    }
+
+    @FXML
+    private void initialize() {
+        // Panggil metode untuk mengatur data ke dalam label saat halaman dibuka
+        setData();
+    }
+
 }
